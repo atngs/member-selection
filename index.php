9078db1d72cb3664bce39d26ecd2a9a30dac891e
at <yanayanoatsushi@gmail.com>
2018-04-26 23:51:27 +0900

    display member_count

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
     <?php require_once("data.php") ?>
     <div class="member-wrapper container">
         <h1 class="logo">keyakizaka46</h1>
+        <p>メンバー：<?php echo Member::getCount() ?>人</p>
         <form action="./confirm_select_member.php">
         <div class="keyakizaka-members">
             <?php foreach($members as $member)://data.phpの配列 ?>
--- member.php
+++ member.php
@@ -3,11 +3,13 @@
     private $name;
     private $profileImage;
 
+    private static $count = 0;
+
     //インスタンス生成毎に実行
     public function __construct($name,$profileImage){
         $this->name = $name;
         $this->profileImage = $profileImage;
-
+        self::$count++;
     }
 
     public function getName(){
@@ -15,6 +17,10 @@
     }
     public function getProfileImage(){
         return $this->profileImage;
+    }
+
+    public static function getCount(){
+        return self::$count;
     }
  }
 ?>
